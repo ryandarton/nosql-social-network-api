2b28f70f8170af75924b6d0d27009bd3f6a888dc
Ryan Darton <ryan@timemachinevideo.com>
2024-03-30 16:40:15 -0600

    Add route to get a single user by _id

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,4 +12,18 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET a single user by _id
+router.get('/:userId', async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId }).populate('thoughts friends');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
