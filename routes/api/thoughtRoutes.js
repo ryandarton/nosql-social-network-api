2a9b30e5ca95b75748ce59139832aa4d55833a61
Ryan Darton <ryan@timemachinevideo.com>
2024-03-30 16:59:15 -0600

    Add route to get a single thought by _id

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -12,4 +12,18 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET a single thought by _id
+router.get('/:thoughtId', async (req, res) => {
+  try {
+    const thought = await Thought.findOne({ _id: req.params.thoughtId }).populate('reactions');
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json(thought);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
